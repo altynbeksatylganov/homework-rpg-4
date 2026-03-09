c01c9d7ce271074356ed1b410142809d8144774e
Baubek Ukibassov <ukibas.b@gmail.com>
2026-03-09 20:06:25 +0500

    Initial commit

---
+++ src/com/narxoz/rpg/bridge/SingleTargetSkill.java
@@ -0,0 +1,16 @@
+package com.narxoz.rpg.bridge;
+
+import com.narxoz.rpg.composite.CombatNode;
+
+public class SingleTargetSkill extends Skill {
+    public SingleTargetSkill(String skillName, int basePower, EffectImplementor effect) {
+        super(skillName, basePower, effect);
+    }
+
+    @Override
+    public void cast(CombatNode target) {
+        // TODO: Single-target Bridge action
+        // 1) Resolve final damage through effect implementor
+        // 2) Apply to target node
+    }
+}
